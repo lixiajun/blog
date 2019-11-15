4bafdde59e4a8338db17e0a5b55db51c58cff9c0
unknown <727996362@qq.com>
2019-11-15 12:57:13 +0800

    django2迁移--更待resolve

--- blog/article/models.py
+++ blog/article/models.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone   # settings中的TIME_ZONE要改为'Asia/Shanghai'
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 # Create your models here.
 
 
